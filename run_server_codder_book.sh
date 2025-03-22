6efb43d1b389e4bf8bb10f33c64adfd67b8ac5d3
crutoboy <i@crutoboy.ru>
2025-03-22 17:21:32 +0000

    fix small mistakes

--- run_server_codder_book.sh
+++ run_server_codder_book.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
 
-# Проверка, что передан аргумент
-if [ -z "$1" ]; then
-  echo "Ошибка: укажите путь к рабочей области."
-  exit 1
-fi
-
-WORKSPACE_PATH="$1"
-
-# Проверка существования директории рабочей области
-if [ ! -d "$WORKSPACE_PATH" ]; then
-  echo "Ошибка: рабочая область '$WORKSPACE_PATH' не существует."
-  exit 1
-fi
-
-# Проверка существования виртуального окружения
-if [ ! -d "$WORKSPACE_PATH/.venv" ]; then
-  echo "Ошибка: виртуальное окружение не найдено в '$WORKSPACE_PATH/.venv'."
-  exit 1
-fi
-
-cd $WORKSPACE_PATH
-exec $WORKSPACE_PATH/.venv/bin/python3 $WORKSPACE_PATH/manage.py runserver 127.0.0.1:8000
+exec ./.venv/bin/python3 ./manage.py runserver 127.0.0.1:8000
